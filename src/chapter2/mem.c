966d4ee3c9004e89f9967110429ba55d0edd422c
shuya <shuya@sfc.wide.ad.jp>
2017-10-19 18:14:41 +0900

    add chapter2 slide

--- src/chapter2/mem.c
+++ src/chapter2/mem.c
@@ -6,15 +6,15 @@
 int
 main (int argc, char *argv[])
 {
-    int *p = malloc(sizeof(int));                   //a1
+    int *p = malloc(sizeof(int));                   
     assert(p != NULL);
     printf ("(%d) address pointed to by p: %p\n", 
-        getpid(), p);                               //a2
-    *p = 0;                                         //a3
+        getpid(), p);                               
+    *p = 0;                                         
     while(1) {
         Spin (1);
         *p = *p + 1;
-        printf("(%d) p: %d\n", getpid(), *p);       //a4 v 
+        printf("(%d) p: %d\n", getpid(), *p);       
     }
     return 0;
 }
